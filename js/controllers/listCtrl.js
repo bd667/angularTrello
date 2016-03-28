dde9cb3c6637e266c32d1a678beaee17b66f8d9c
Kirill <bd667@mail.ru>
2016-03-28 14:34:32 +0300

    angular test trello

---
+++ js/controllers/listCtrl.js
@@ -0,0 +1,17 @@
+
+angular.module('app').controller('listCtrl', function(listFactory, cardFactory){
+    console.log('listCtrl');
+    this.removeList = function(list){
+        console.log('list', list);
+        listFactory.removeList(list);
+    };
+
+    this.getCards = function(list) {
+        return cardFactory.getCards(list);
+    };
+
+    this.createCard = function(list) {
+        cardFactory.createCard(list, this.cardDescription);
+        this.cardDescription = '';
+    };
+});
